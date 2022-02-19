d11ef1f1e2d80bdbae037429c659da2c8d831576
RyzeCodes <93492656+RyzeCodes@users.noreply.github.com>
2022-02-19 12:58:14 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,130 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
-let engine;
-let world;
-var ball;
-var ground;
-var wedge;
-var angle=60;
-var poly;
-var boxes=[];
+var engine, world;
+var canvas;
+var palyer, playerBase;
+var computer, computerBase;
+
+//Declare an array for arrows playerArrows = [ ]
+var playerArrows = [];
+
+var arrow;
+
 
 function setup() {
-  createCanvas(400,400);
+  canvas = createCanvas(windowWidth, windowHeight);
 
   engine = Engine.create();
   world = engine.world;
-  var option={
-    isStatic:true
-  };
-  ground=Bodies.rectangle(100,300,400,20);
-  World.add(world,ground);
-  box1 = new Box(200,100,50,50);
+
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
+
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
+  computerArcher = new ComputerArcher(
+    width - 340,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
   
+ 
 
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
-}
-function mousePressed(){
-  boxes.push(
-    
-    
-    new Box(50,50,mouseX,mouseY)
-    
-    
-    )
+
 }
 
-function draw() 
-{
-  background(51);
-  rect(ground.position.x,ground.position.y,400,10);
+function draw() {
+  background(180);
+
   Engine.update(engine);
- for(var i=0;i<boxes.length;i++) {
- boxes[i].show();}
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+ 
+  playerBase.display();
+  player.display();
   
+
+  computerBase.display();
+  computer.display();
   
+  playerArcher.display();
+  computerArcher.display()
+
+ 
+ for (var i=0; i<playerArrows; i++) 
+ {
+ showArrows(i, playerArrows);
+ }
+
+
+
+
+
+
 }
 
+/*********** Choose correct keyPressed() function out of these *************/
+
+function keyPressed() {
+  if(keyCode === 32){
+    // create an arrow object and add into an array ; set its angle same as angle of playerArcher
+    var posX = playerArcher.body.position.x;
+    var posY = playerArcher.body.position.y;
+    var angle = playerArcher.body
+    var arrow = new PlayerArrow(posX, posY, 100, 10);
+    arrow.trajectory = [];
+    Matter.Body.setAngle(arrow.body);
+    playerArrows.push(arrow);
+  }
+}
+
+
+
+
+
+
+function keyReleased () {
+
+  if(keyCode === 32){
+    //call shoot() function for each arrow in an array playerArrows
+    if (playerArrows.length) {
+      var angle = playerArcher.body.angle+PI/2;
+      playerArrows[playerArrows.length - 1].shoot(angle);
+    }
+  }
+
+}
+//Display arrow and Tranjectory
+function showArrows(index, arrows) {
+  arrows[index].display();
+  
+    
+  
+ 
+
+}
