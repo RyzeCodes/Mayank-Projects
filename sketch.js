1fe8e015dd16f186d5d5ca0130be5a5f696a9a32
RyzeCodes <93492656+RyzeCodes@users.noreply.github.com>
2022-03-18 19:00:27 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,130 +1,76 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
 
-var engine, world;
-var canvas;
-var palyer, playerBase;
-var computer, computerBase;
+function preload(){
+   bg =loadImage("Images/cityImage.png");
+   balloonImage1=loadAnimation("Images/HotAirBallon-01.png");
+   balloonImage2=loadAnimation("Images/HotAirBallon-01.png","Images/HotAirBallon-01.png",
+   "Images/HotAirBallon-01.png","Images/HotAirBallon-02.png","Images/HotAirBallon-02.png",
+   "Images/HotAirBallon-02.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png");
+  }
 
-//Declare an array for arrows playerArrows = [ ]
-var playerArrows = [];
-
-var arrow;
-
-
+//Function to set initial environment
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  database=firebase.database();
+  createCanvas(1500,700);
 
-  engine = Engine.create();
-  world = engine.world;
+  balloon=createSprite(250,650,250,650);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
 
-  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
-  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
-  playerArcher = new PlayerArcher(
-    340,
-    playerBase.body.position.y - 180,
-    120,
-    120
-  );
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, console.log("error"));
+  textSize(20); 
+}
 
-  computerBase = new ComputerBase(
-    width - 300,
-    random(450, height - 300),
-    180,
-    150
-  );
-  computer = new Computer(
-    width - 280,
-    computerBase.body.position.y - 153,
-    50,
-    180
-  );
-  computerArcher = new ComputerArcher(
-    width - 340,
-    computerBase.body.position.y - 180,
-    120,
-    120
-  );
-  
- 
+// function to display UI
+function draw() {
+  background(bg);
 
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    //add the animation of balloon [use balloonImage2]
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    //add the animation of balloon [use balloonImage2]
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+ //add the animation of balloon [use balloonImage2]
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+  //add the animation of balloon [use balloonImage2]
+    balloon.scale=balloon.scale+0.005;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
 
 }
 
-function draw() {
-  background(180);
 
-  Engine.update(engine);
+function updateHeight(x,y){
+  database.ref('/balloon/height').update({
+    'x': height.x + x ,
+    'y': height.y + y
+  })
+}
 
-  // Title
-  fill("#FFFF");
-  textAlign("center");
-  textSize(40);
-  text("EPIC ARCHERY", width / 2, 100);
 
- 
-  playerBase.display();
-  player.display();
-  
 
-  computerBase.display();
-  computer.display();
-  
-  playerArcher.display();
-  computerArcher.display()
 
- 
- for (var i=0; i<playerArrows; i++) 
- {
- showArrows(i, playerArrows);
+function readHeight(data){
+  //assign the value of data to height
+  //assign the x and y value of height to the respective x and y position of balloon
  }
 
-
-
-
-
-
-}
-
-/*********** Choose correct keyPressed() function out of these *************/
-
-function keyPressed() {
-  if(keyCode === 32){
-    // create an arrow object and add into an array ; set its angle same as angle of playerArcher
-    var posX = playerArcher.body.position.x;
-    var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body
-    var arrow = new PlayerArrow(posX, posY, 100, 10);
-    arrow.trajectory = [];
-    Matter.Body.setAngle(arrow.body);
-    playerArrows.push(arrow);
-  }
-}
-
-
-
-
-
-
-function keyReleased () {
-
-  if(keyCode === 32){
-    //call shoot() function for each arrow in an array playerArrows
-    if (playerArrows.length) {
-      var angle = playerArcher.body.angle+PI/2;
-      playerArrows[playerArrows.length - 1].shoot(angle);
-    }
-  }
-
-}
-//Display arrow and Tranjectory
-function showArrows(index, arrows) {
-  arrows[index].display();
-  
-    
-  
- 
-
+function showError(){
+  console.log("Error in writing to the database");
 }
